a9486d498d8e58060e63e5f5d158d1dafdee2b55
Robbie Jaeger <jaeger.rob@gmail.com>
2016-03-31 17:13:43 -0600

    Added swap method and test for bubble

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,6 +1,22 @@
 class BubbleSort
 
   def sort(array)
+    if array.length > 1
+      swapped = false
+      while swapped == false
+        array.each_index do |index|
+          #
+        end
+      end
+    end
+    array
+  end
+
+  # private
+
+  def swap(array, swap_right, swap_left)
+      array[swap_right], array[swap_left] = array[swap_left], array[swap_right]
+      array
   end
 
 end
--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -9,4 +9,31 @@ class BubbleSortTest < Minitest::Test
     assert sorter
   end
 
+  def test_empty_array_is_returned_as_sorted
+    sorter = BubbleSort.new
+    array = []
+
+    sorted = sorter.sort(array)
+
+    assert_equal [], sorted
+  end
+
+  def test_array_of_one_is_returned_as_sorted
+    sorter = BubbleSort.new
+    array = [1]
+
+    sorted = sorter.sort(array)
+
+    assert_equal [1], sorted
+  end
+
+  def test_swap
+    sorter = BubbleSort.new
+
+    array = [2, 3, 7, 4]
+
+    swapped = sorter.swap(array, 0, 3)
+
+    assert_equal [4, 3, 7, 2], swapped
+  end
 end
