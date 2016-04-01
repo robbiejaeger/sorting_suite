a9ea8df97e9d1b1d01e4421bcbee8d389a03b569
Robbie Jaeger <jaeger.rob@gmail.com>
2016-03-31 21:54:51 -0600

    Added basic insertion tests and began sort method

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,6 +1,23 @@
 class InsertionSort
 
   def sort(array)
+    array = clean_array(array)
+    return array if array.size < 2
+    sorted_array = []
+
+    
+  end
+
+
+  private
+
+  def clean_array(array)
+    array.reject {|i| i.to_s.empty?}
+  end
+
+  def swap(array, swap_right, swap_left)
+      array[swap_right], array[swap_left] = array[swap_left], array[swap_right]
+      array
   end
 
 end
