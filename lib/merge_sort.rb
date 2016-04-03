7ca6f8ab62daf34d44ae7c5b7b6da465e24099d4
Robbie Jaeger <jaeger.rob@gmail.com>
2016-04-03 08:22:13 -0600

    Adding merge method, but not working yet...

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -14,4 +14,16 @@ class MergeSort
     array.reject {|i| i.to_s.empty?}
   end
 
+  def merge_arrays(left_array, right_array)
+    if left_array == []
+      right_array
+    elsif right_array == []
+      left_array
+    elsif left_array[0] < right_array[0]
+      left_array[0] + merge_arrays(left_array[1.upto(left_array.count)], right_array)
+    elsif left_array[0] > right_array[0]
+      right_array[0] + merge_arrays(left_array, right_array[1.upto(right_array.count)])
+    end
+  end
+
 end
--- test/merge_sort_test.rb
+++ test/merge_sort_test.rb
@@ -2,6 +2,10 @@ require "minitest/autorun"
 require "minitest/pride"
 require "merge_sort"
 
+class MergeSort
+  public :merge_arrays
+end
+
 class MergeSortTest < Minitest::Test
 
   def test_class_exists
@@ -27,4 +31,13 @@ class MergeSortTest < Minitest::Test
     assert_equal [1], sorted
   end
 
+  def test_can_merge_arrays
+    sorter = MergeSort.new
+    left_array = [1, 3]
+    right_array = [2, 4]
+
+    merged = sorter.merge_arrays(left_array, right_array)
+
+    assert_equal [1, 2, 3, 4], merged
+  end
 end
