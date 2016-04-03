c43f810ff809e4154772113e86f82b4f00e39b18
Robbie Jaeger <jaeger.rob@gmail.com>
2016-04-03 07:59:11 -0600

    Add basic tests and array cleanup

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,6 +1,17 @@
 class MergeSort
 
   def sort(array)
+    array = clean_array(array)
+    return array if array.size < 2
+
+    
+  nd
+
+
+  private
+
+  def clean_array(array)
+    array.reject {|i| i.to_s.empty?}
   end
 
 end
--- test/merge_sort_test.rb
+++ test/merge_sort_test.rb
@@ -9,4 +9,22 @@ class MergeSortTest < Minitest::Test
     assert sorter
   end
 
+  def test_empty_array_is_returned_as_sorted
+    sorter = MergeSort.new
+    array = []
+
+    sorted = sorter.sort(array)
+
+    assert_equal [], sorted
+  end
+
+  def test_array_of_one_is_returned_as_sorted
+    sorter = MergeSort.new
+    array = [1]
+
+    sorted = sorter.sort(array)
+
+    assert_equal [1], sorted
+  end
+
 end
